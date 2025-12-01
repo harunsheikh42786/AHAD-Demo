f7a794d5208252213230935ce16dd70856e627b4
Harun <harunsheikh42786@gmail.com>
2025-12-01 18:40:12 +0530

    17

---
+++ src/main/java/com/ahad/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ahad.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home"; // Assuming you have a view named "home"
+    }
+}
